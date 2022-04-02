c8b3574bbbb14e455104a49c03254ab13fd877a1
Xetera <mythologyali@gmail.com>
2022-04-03 02:44:01 +0300

    fixed background mask

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -3,19 +3,24 @@ import requests
 from io import BytesIO
 
 with open("./reference.png") as file:
+    SCALING = 3
     img = Image.open(file.buffer)
-    img = img.resize((img.size[0], img.size[1]), Image.NEAREST)
+    img = img.resize((img.size[0] * SCALING, img.size[1] * SCALING), Image.NEAREST)
 
     mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
     response = requests.get(mask_url)
     mask_i = Image.open(BytesIO(response.content))
-    mask = Image.new("1", (2000, 1000), 0)
-    mask.paste(mask_i)
+    mask = Image.new("1", (2000 * SCALING, 1000 * SCALING), 0)
+    large_mask = Image.new("1", (2000 * SCALING, 1000 * SCALING), 0)
+    mask.resize((mask_i.size[0], mask_i.size[1] * 2), Image.NEAREST)
+    mask.paste(mask_i, (0, 0))
+    mask.paste(mask_i, (mask_i.width, 0))
 
-    tl = (1607, 880) # top left corner
+    tl = (1607 * SCALING, 880 * SCALING) # top left corner
+    mask.save("mask.png")
 
-    final_img = Image.new('RGBA', (2000, 1000))
-    unmasked_img = Image.new('RGBA', (2000, 1000))
+    final_img = Image.new('RGBA', (2000 * SCALING, 1000 * SCALING))
+    unmasked_img = Image.new('RGBA', (2000 * SCALING, 1000 * SCALING))
     unmasked_img.paste(img, tl)
     final_img = Image.composite(final_img, unmasked_img, mask)
     final_img.save("red_velvet_overlay.png")
