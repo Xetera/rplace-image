3b6b7ff7659a6d60b1ace163694cec62e7c5ebd5
Xetera <mythologyali@gmail.com>
2022-04-03 01:56:47 +0300

    attempting to fix image boundaries

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -1,22 +1,22 @@
-from PIL import Image, ImageOps
+from PIL import Image
 import requests
 from io import BytesIO
 
-response = requests.get("https://raw.githubusercontent.com/xetera/rplace-red-velvet/main/reference.png")
-img = Image.open(BytesIO(response.content))
-img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
+with open("./reference.png") as file:
+    img = Image.open(file.buffer)
+    img = img.resize((img.size[0] * 6, img.size[1] * 6), Image.NEAREST)
 
-mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
-response = requests.get(mask_url)
-mask_i = Image.open(BytesIO(response.content))
-mask = Image.new("1", (3000, 3000), 0)
-mask.paste(mask_i)
+    mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
+    response = requests.get(mask_url)
+    mask_i = Image.open(BytesIO(response.content))
+    mask = Image.new("1", (6000, 3000), 0)
+    mask.paste(mask_i)
 
-tl = (1607 * 3, 880  * 3) # top left corner
+    tl = (1607 * 3, 880  * 3) # top left corner
 
-final_img = Image.new('RGBA', (3000, 3000))
-unmasked_img = Image.new('RGBA', (3000, 3000))
-unmasked_img.paste(img, tl)
-final_img = Image.composite(final_img, unmasked_img, mask)
-final_img.save("red_velvet_overlay.png")
+    final_img = Image.new('RGBA', (6000, 3000))
+    unmasked_img = Image.new('RGBA', (6000, 3000))
+    unmasked_img.paste(img, tl)
+    final_img = Image.composite(final_img, unmasked_img, mask)
+    final_img.save("red_velvet_overlay.png")
 
